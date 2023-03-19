ee92e6f3b40034d8c6b8ee374f726489017cac4f
bhanuprasanna527 <79924017+bhanuprasanna527@users.noreply.github.com>
2023-03-20 05:05:58 +0530

    Update

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/snippets', title: 'Snippets' },
   { href: '/resources', title: 'Resources' },
+  { href: '/log', title: 'Log' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
 ]
