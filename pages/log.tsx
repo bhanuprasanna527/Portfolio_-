e45c1b4807b3e48461c6684269b3aa94ae6712aa
bhanuprasanna527 <79924017+bhanuprasanna527@users.noreply.github.com>
2023-03-20 20:13:03 +0530

    Update

--- pages/log.tsx
+++ pages/log.tsx
@@ -5,10 +5,7 @@ import { PageSEO } from '@/components/SEO'
 export default function Snippets() {
   return (
     <>
-      <PageSEO
-        title={`Resources - ${siteMetadata.author}`}
-        description={siteMetadata.description}
-      />
+      <PageSEO title={`Log - ${siteMetadata.author}`} description={siteMetadata.description} />
 
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-background-color dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
