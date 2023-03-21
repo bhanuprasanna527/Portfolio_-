14789c4f43883a1cf10d49aacf421c0afb800fa5
bhanuprasanna527 <79924017+bhanuprasanna527@users.noreply.github.com>
2023-03-21 14:31:29 +0530

    Update

--- components/Card.tsx
+++ components/Card.tsx
@@ -44,7 +44,7 @@ const Card = ({ title, description, imgSrc, href }) => (
         {href && (
           <Link
             href={href}
-            className="text-base font-medium leading-6 text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            className="text-base font-medium leading-6 text-[#ea580c] hover:text-[#f97316] dark:hover:text-[#fb923c]"
             aria-label={`Link to ${title}`}
           >
             Learn more &rarr;
