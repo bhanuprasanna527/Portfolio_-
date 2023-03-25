ae4a3ce2165e4cfd0de06c1e263603ab4334b697
bhanuprasanna527 <79924017+bhanuprasanna527@users.noreply.github.com>
2023-03-25 10:23:58 +0530

    Update

--- pages/links.tsx
+++ pages/links.tsx
@@ -1,7 +1,7 @@
-import links from '@/data/links'
-import clsx from 'clsx'
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillGithub, AiFillCompass } from 'react-icons/ai'
+import { SiGooglescholar } from 'react-icons/si'
 
 export default function Links() {
   return (
@@ -12,19 +12,55 @@ export default function Links() {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Links
           </h1>
+          <a
+            key="https://twitter.com/bhanu527"
+            href="https://twitter.com/bhanu527"
+            target="__blank"
+            className="mb-4 flex w-full flex-row items-center space-x-2 rounded-md border-2 border-gray-200 py-2 hover:bg-gray-50 dark:border-zinc-800 dark:hover:bg-zinc-800"
+          >
+            <span className="mr-2 inline-block"></span> <AiFillTwitterCircle className="h-5 w-5" />
+            <span className="font-bold">Twitter</span>
+          </a>
 
-          {links?.map((el) => (
-            <a
-              key={el?.href}
-              href={el?.href}
-              target="__blank"
-              className="mb-4 flex w-full flex-row items-center space-x-2 rounded-md border-2 border-gray-200 py-2 hover:bg-gray-50 dark:border-zinc-800 dark:hover:bg-zinc-800"
-            >
-              <span className="mr-2 inline-block">{el?.icon}</span>{' '}
-              <el.icon className={clsx('h-5 w-5', el?.iconClass)} />
-              <span className="font-bold">{el?.name}</span>
-            </a>
-          ))}
+          <a
+            key="https://kbpb.netlify.app/"
+            href="https://kbpb.netlify.app/"
+            target="__blank"
+            className="mb-4 flex w-full flex-row items-center space-x-2 rounded-md border-2 border-gray-200 py-2 hover:bg-gray-50 dark:border-zinc-800 dark:hover:bg-zinc-800"
+          >
+            <span className="mr-2 inline-block"></span> <AiFillCompass className="h-5 w-5" />
+            <span className="font-bold">Portfolio</span>
+          </a>
+
+          <a
+            key="https://linkedin.com/in/bhanuprasanna527"
+            href="https://linkedin.com/in/bhanuprasanna527"
+            target="__blank"
+            className="mb-4 flex w-full flex-row items-center space-x-2 rounded-md border-2 border-gray-200 py-2 hover:bg-gray-50 dark:border-zinc-800 dark:hover:bg-zinc-800"
+          >
+            <span className="mr-2 inline-block"></span> <AiFillLinkedin className="h-5 w-5" />
+            <span className="font-bold">LinkedIn</span>
+          </a>
+
+          <a
+            key="https://github.com/bhanuprasanna527"
+            href="https://github.com/bhanuprasanna527"
+            target="__blank"
+            className="mb-4 flex w-full flex-row items-center space-x-2 rounded-md border-2 border-gray-200 py-2 hover:bg-gray-50 dark:border-zinc-800 dark:hover:bg-zinc-800"
+          >
+            <span className="mr-2 inline-block"></span> <AiFillGithub className="h-5 w-5" />
+            <span className="font-bold">GitHub</span>
+          </a>
+
+          <a
+            key="https://scholar.google.com/citations?user=Z9gKj9AAAAAJ&hl=en"
+            href="https://scholar.google.com/citations?user=Z9gKj9AAAAAJ&hl=en"
+            target="__blank"
+            className="mb-4 flex w-full flex-row items-center space-x-2 rounded-md border-2 border-gray-200 py-2 hover:bg-gray-50 dark:border-zinc-800 dark:hover:bg-zinc-800"
+          >
+            <span className="mr-2 inline-block"></span> <SiGooglescholar className="h-5 w-5" />
+            <span className="font-bold">Google Scholar</span>
+          </a>
         </div>
       </div>
     </>
